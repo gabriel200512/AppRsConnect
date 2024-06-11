32638f775aef361b5b53f817e836d2f13f6ce6c4
gabriel200512 <ffgabriel905@gmail.com>
2024-06-10 22:14:34 -0300

    bottom

--- app/src/main/java/br/com/etecia/apprsconnect/MainActivity.java
+++ app/src/main/java/br/com/etecia/apprsconnect/MainActivity.java
@@ -1,21 +1,51 @@
 package br.com.etecia.apprsconnect;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
+import android.view.MenuItem;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        // Set up the toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        // Handle home action
+                        return true;
+                    case R.id.action_people:
+                        // Handle people action
+                        return true;
+                    case R.id.action_search:
+                        // Handle search action
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.scroll_view), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
---
+++ app/src/main/java/br/com/etecia/apprsconnect/NonNull.java
@@ -0,0 +1,4 @@
+package br.com.etecia.apprsconnect;
+
+public @interface NonNull {
+}
