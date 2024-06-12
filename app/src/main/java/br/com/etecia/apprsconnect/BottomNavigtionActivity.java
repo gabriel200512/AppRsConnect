75af0a8ba25c450143bcf43caf0973a3547cfafe
gabriel200512 <ffgabriel905@gmail.com>
2024-06-11 21:45:03 -0300

    so vai

--- app/src/main/java/br/com/etecia/apprsconnect/BottomNavigtionActivity.java
+++ app/src/main/java/br/com/etecia/apprsconnect/BottomNavigtionActivity.java
@@ -1,5 +1,6 @@
 package br.com.etecia.apprsconnect;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,12 +11,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class BottomNavigtionActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.bottom_navigtion_layout);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottom_navigation), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- app/src/main/java/br/com/etecia/apprsconnect/MainActivity.java
+++ app/src/main/java/br/com/etecia/apprsconnect/MainActivity.java
@@ -1,9 +1,10 @@
 package br.com.etecia.apprsconnect;
 
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
@@ -18,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         // Set up the toolbar
@@ -40,8 +40,9 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.action_search:
                         // Handle search action
                         return true;
+                    default:
+                        return false;
                 }
-                return false;
             }
         });
 
